333b0b249addcdb7274943842aad942811d7b7ae
Jay-716 <jay13422525511@gmail.com>
2022-07-12 11:47:28 +0800

    Edited .gitignore. Main screen layout finished.

--- app/src/main/java/com/lightnote/MainActivity.java
+++ app/src/main/java/com/lightnote/MainActivity.java
@@ -2,7 +2,11 @@ package com.lightnote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent jmp_to_maincontent = new Intent(this, NoteMain.class);
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(jmp_to_maincontent);
+            }
+        };
+        timer.schedule(task, 1000);
     }
 }
