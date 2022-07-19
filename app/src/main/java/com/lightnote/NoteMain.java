b9935a2c0d02365c213c7c266b9659ed788db188
Jay-716 <jay13422525511@gmail.com>
2022-07-19 23:01:46 +0800

    Edit .gitignore. Add some buttons.

--- app/src/main/java/com/lightnote/NoteMain.java
+++ app/src/main/java/com/lightnote/NoteMain.java
@@ -3,12 +3,58 @@ package com.lightnote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class NoteMain extends AppCompatActivity {
+
+    private EditText searchbar;
+    private TextView addnote;
+
+    //Prevent return to start activity
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_main);
+
+        //Search event
+        searchbar = findViewById(R.id.searchbar);
+        searchbar.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getKeyCode() == 66){
+                    search(searchbar.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //Add note event
+        addnote = findViewById(R.id.sidebar_tv_add);
+        addnote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNote();
+            }
+        });
+    }
+
+    //Search note
+    protected void search(String str){
+        Toast.makeText(this, "Search hit!", Toast.LENGTH_SHORT).show();
+    }
+
+    //Add note
+    protected void addNote(){
+        Toast.makeText(this, "Add note.", Toast.LENGTH_SHORT).show();
     }
 }
